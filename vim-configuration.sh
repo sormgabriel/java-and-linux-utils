25435dc83d5b0349754d77d8821766936588cdae
sormgabriel <sormgabriel@gmail.com>
2018-04-07 15:30:52 -0300

    Se cambia el script para que escriba el .vimrc en el home

--- vim-configuration.sh
+++ vim-configuration.sh
@@ -4,7 +4,7 @@ mkdir -p ~/.vim/autoload ~/.vim/bundle && \
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
 touch ~/ .vimrc
-
+cd ~/
 echo "execute pathogen#infect()
 syntax on
 filetype plugin indent on"<.vimrc
